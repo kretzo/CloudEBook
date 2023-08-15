e944212fcbae028e33c5670e08ce842f8452448a
kretzo <134546778+kretzo@users.noreply.github.com>
2023-08-15 17:12:49 -0300

    Atualização- 15/08

---
+++ README.md
@@ -0,0 +1,2 @@
+# Projeto_14_08
+Integrantes: Guilherme Swiatyi, João Vitor Joslin, João Vítor Kretzschmar
--- cadastro.js
+++ cadastro.js
@@ -1,3 +1,5 @@
+var Usuarios = [];
+
 function Cadastro(){
     nome = document.getElementById('nomeCad').value;
     sobrenome = document.getElementById('sobrenomeCad').value;
@@ -5,13 +7,37 @@ function Cadastro(){
     senha = document.getElementById('senhaCad').value;
     senhaVerif = document.getElementById('senhaCad2').value;
 
-    if(senhaVerif != senha){
-        alert('As senhas não coincidem!');
+    if(Usuarios.includes(email)){
+        alert('Email já cadastrado!');
     }
+
     else{
-        console.log(`Nome: ${nome}`);
-        console.log(`Sobrenome: ${sobrenome}`);
+        if(senhaVerif != senha){
+            alert('As senhas não coincidem!');
+        }
+        else{
+
+            Usuarios.push(nome, sobrenome, email, senha);
+
+            console.log(`Nome: ${nome}`);
+            console.log(`Sobrenome: ${sobrenome}`);
+            console.log(`Email: ${email}`);
+            console.log(`Senha: ${senha}`);
+        }
+    }
+}
+
+function Login(){
+
+    let email = document.getElementById('emailLogin').value;
+    let senha = document.getElementById('senhaLogin').value;
+
+    if(Usuarios.includes(email) && (senha)){
+        alert(`Bem-vindo, ${nome}`);
         console.log(`Email: ${email}`);
         console.log(`Senha: ${senha}`);
+    }
+    else{
+        alert('Usuário não encontrado!');
     }
 }
