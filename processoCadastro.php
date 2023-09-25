1e508203f6347cdabbedce5ece136c7494c71f97
kretzo <134546778+kretzo@users.noreply.github.com>
2023-09-25 16:54:38 -0300

    25/09

--- processoCadastro.php
+++ processoCadastro.php
@@ -1,33 +1,55 @@
 <?php
-    class Cadastro{
+    // class Cadastro{
+    //     private $inserir;
+    //     public function __construct() {
+    //         $nome = $_POST['nomeCad'];
+    //         $sobrenome = $_POST['sobrenomeCad'];
+    //         $email = $_POST['emailCad'];
+    //         $senha = $_POST['senhaCad'];
+    //         $senhaVerif = $_POST['senhaCad2'];
 
-        private $inserir;
+    //         if($senha == $senhaVerif){
+    //             include('conexao.php');
 
-        public function __construct() {
-            $nome = $_POST['nomeCad'];
-            $sobrenome = $_POST['sobrenomeCad'];
-            $email = $_POST['emailCad'];
-            $senha = $_POST['senhaCad'];
-            $senhaVerif = $_POST['senhaCad2'];
-
-            if($senha == $senhaVerif){
-                include('conexao.php');
-
-                if($nome != "" && $sobrenome != "" && $email != "" && $senha != "" && $senhaVerif != ""){
-                    $str = "INSERT INTO tb_usuario (Nome, Sobrenome, Email, Senha) VALUES ('$nome', '$sobrenome', '$email', '$senha')";
+    //             if($nome != "" && $sobrenome != "" && $email != "" && $senha != "" && $senhaVerif != ""){
+    //                 $str = "INSERT INTO tb_usuario (Nome, Sobrenome, Email, Senha) VALUES ('$nome', '$sobrenome', '$email', '$senha')";
                     
-                    $this -> $inserir = mysqli_query($conectar, $str);
+    //                 $this -> $inserir = mysqli_query($conec, $str);
 
-                    header("Location: login.html");
-                    exit();
-                }
-                else{
-                    echo("Preencha todos os campos!");
-                }
-            }
-            else{
-                echo("As senhas não coincidem!");
-            }
+    //                 header("Location: login.html");
+    //                 exit();
+    //             }
+    //             else{
+    //                 echo("Preencha todos os campos!");
+    //             }
+    //         }
+    //         else{
+    //             echo("As senhas não coincidem!");
+    //         }
+    //     }
+    // }
+
+    $nome = $_POST['nomeCad'];
+    $sobrenome = $_POST['sobrenomeCad'];
+    $email = $_POST['emailCad'];
+    $senha = $_POST['senhaCad'];
+    $senhaVerif = $_POST['senhaCad2'];
+
+    if($nome != "" && $sobrenome != "" && $email != "" && $senha != "" && $senhaVerif != ""){
+        if($senha == $senhaVerif){
+            include('conexao.php');
+
+            $str = "INSERT INTO tb_usuario (Nome, Sobrenome, Email, Senha) VALUES ('$nome', '$sobrenome', '$email', '$senha')";
+            $inserir = mysqli_query($conectar, $str);
+            
+            header("Location: login.html");
+            exit();
         }
+        else{
+            echo "<script>alert('As senhas não coincidem!')</script>";
+        }
+    }
+    else{
+        echo "<script>alert('Preencha todos os campos!')</script>";
     }
 ?>
--- processoLogin.php
+++ processoLogin.php
@@ -1,32 +1,55 @@
 <?php
-    class Login{
+    // class Login{
 
-        private $count;
+    //     private $count;
 
-        public function __construct(){
-            $email = $_POST['emailLogin'];
-            $senha = $_POST['senhaLogin'];
+    //     public function __construct(){
+    //         $email = $_POST['emailLogin'];
+    //         $senha = $_POST['senhaLogin'];
 
-            if($email != "" && $senha != ""){
-                include('conexao.php');
+    //         if($email != "" && $senha != ""){
+    //             include('conexao.php');
 
-                $verific = "SELECT IdUsuario FROM tb_usuario WHERE Email = '$email' AND Senha = '$senha'";
-                $loginVerif = mysqli_query($conectar, $verific);
+    //             $verific = "SELECT IdUsuario FROM tb_usuario WHERE Email = '$email' AND Senha = '$senha'";
+    //             $loginVerif = mysqli_query($conec, $verific);
                 
-                $this -> $count = mysqli_num_rows($loginVerif);
+    //             $this -> $count = mysqli_num_rows($loginVerif);
 
-                if($count == 1){
-                    header("Location: home.html");
-                    exit;
-                 }
-                 else{
-                     echo "<script>alert('Email ou senha incorretos!')</script>";
-                 }
-            }
+    //             if($count == 1){
+    //                 header("Location: home.html");
+    //                 exit;
+    //              }
+    //              else{
+    //                  echo "<script>alert('Email ou senha incorretos!')</script>";
+    //              }
+    //         }
 
-            else{
-                echo "<script>alert('Preencha todos os dados!')</script>";
-            }
-         }
+    //         else{
+    //             echo "<script>alert('Preencha todos os dados!')</script>";
+    //         }
+    //      }
+    // }
+
+    $email = $_POST['emailLogin'];
+    $senha = $_POST['senhaLogin'];
+
+    if($email != "" && $senha != ""){
+        include('conexao.php');
+
+        $verific = "SELECT IdUsuario FROM tb_usuario WHERE Email = '$email' AND Senha = '$senha'";
+        $loginVerif = mysqli_query($conectar, $verific);
+
+        $count = mysqli_num_rows($loginVerif);
+
+        if($count == 1){
+           header("Location: home.html");
+           exit;
+        }
+        else{
+            echo "<script>alert('Email ou senha incorretos!')</script>";
+        }
+    }
+    else{
+        echo "<script>alert('Preencha todos os dados!')</script>";
     }
 ?>
